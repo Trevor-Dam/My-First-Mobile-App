eaff295fe1de27e2fe591cae6c0d7b4ba75ce30f
Trevor-Dam <trevordamoyi307@gmail.com>
2024-07-07 16:28:50 +0200

    Adding the sha256 hashing algorithm to the API

---
+++ BudgetAPI/BudgetAPI/Secrecy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Buffers;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BudgetAPI
+{
+    public class Secrecy
+    {
+        public static string hashString(string word)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hValue = Encoding.ASCII.GetBytes(word);
+                byte[] result = SHA256.HashData(hValue);
+                return Encoding.ASCII.GetString(result);
+            }
+        }
+    }
+}
